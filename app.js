05da6d6d54b8dd75ca50bee71018de8e7a649453
Harsh Bhanderi <hbhanderi148@gmial.com>
2023-07-16 14:18:14 +0530

    minor problem fix

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-// const popupS = require("popups");
 
 const app = express();
 
@@ -52,45 +51,24 @@ app.post("/signup", function(req, res){
       });
 
     LoginDetail.find({email: email, username: username}).then(function(foundItem){
-        if(!foundItem){
+        if(foundItem != null){
             res.render("home", {btnValue: details.username});
+            details.save();
         }
         else{
-            res.send("<script>alert('Username or e-mail already in use!<br>Try using another!');window.location = '/';</script>");
+            res.send("<script>alert('Username or e-mail already in use! Try using another!');window.location = '/';</script>");
         }
-    });
-    // LoginDetail.find({username: username}).then(function(foundItem){
-    //     if(!foundItem){
-    //         res.render("home", {btnValue: details.username});
-    //     }
-    //     else{
-    //         res.send("<script>alert('message');window.location = '/';</script>");
-    //     }
-    // });
-
-    details.save();
-
-    // res.render("home", {btnValue: details.username});
+    });  
 });
 
 app.post("/login", function(req, res){
     const email = req.body.email;
     const username = req.body.username;
     const password = req.body.password;
-<<<<<<< HEAD
-    loginDetails.findOne({email: email, password: password}).then(function(err){
-        if(!err){
-            // document.querySelector("button").innerHTML = "qwe";
-            res.redirect("/");
-=======
+
     LoginDetail.findOne({email: email, username: username, password: password}).then(function(foundItem){
         if(foundItem === null){
-<<<<<<< HEAD
-            res.send("<script>alert('Somthing went wring'); window.location.href ='./login.html' </script>");
->>>>>>> 9fae508efdcf1294bc2750395226e1409705ab78
-=======
-            res.send("<script>alert('You may have entered wrong credentials!<br>Please check it try again..');window.location = '/';</script>");
->>>>>>> d7d0c4bd277ccad051f355566265168069165815
+            res.send("<script>alert('You may have entered wrong credentials! Please check it and try again..');window.location = '/';</script>");
         }
         else{
             res.render("home", {btnValue: foundItem.username});
