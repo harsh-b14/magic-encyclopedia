f6807827909642223604d3e28b68fe8588b91d43
Harsh Bhanderi <hbhanderi148@gmial.com>
2023-07-17 17:52:15 +0530

    add template for char and creatures

--- app.js
+++ app.js
@@ -79,3 +79,16 @@ app.post("/login", function(req, res){
 app.listen(3000, function(){
     console.log("server running on 3000");
 });
+
+
+
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+// const msg = {
+//   to: 'test@example.com',
+//   from: 'test@example.com',
+//   subject: 'Sending with Twilio SendGrid is Fun',
+//   text: 'and easy to do anywhere, even with Node.js',
+//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+// };
+// sgMail.send(msg);
