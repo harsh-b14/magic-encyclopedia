947676ab334f763dcbf34f26b3cd293775d90f72
Harsh Bhanderi <hbhanderi148@gmial.com>
2023-07-19 14:33:52 +0530

    minor changes in displaying home page

--- app.js
+++ app.js
@@ -29,55 +29,72 @@ app.get("/", function(req, res){
     res.render("home", {btnValue: "Login"});
 });
 
-// app.get("/signup", function(req, res){
-//     res.sendFile(__dirname + "/login.html");
-// });
-
-app.post("/", function(req, res){   
-    res.sendFile(__dirname + "/login.html");
-});
-
-app.post("/signup", function(req, res){
-    const name = req.body.personName;
-    const email = req.body.email;
-    const username = req.body.username;
-    const password = req.body.password;
-
-    const details = new LoginDetail({
-        name: name,
-        email: email,
-        username: username,
-        password: password
-      });
-
-    LoginDetail.find({email: email, username: username}).then(function(foundItem){
-        if(foundItem != null){
-            res.render("home", {btnValue: details.username});
-            details.save();
-        }
-        else{
-            res.send("<script>alert('Username or e-mail already in use! Try using another!');window.location = '/';</script>");
-        }
-    });  
+app.get("/:userName", function(req, res){
+    const userName = req.params.userName;
+    res.send("hello " + userName);
 });
 
 app.post("/login", function(req, res){
-    const email = req.body.email;
-    const username = req.body.username;
-    const password = req.body.password;
+    const btnValue = req.body.button;
+    if(btnValue == "Login"){
+        res.sendFile(__dirname + "/login.html");
+    }
+    else{
+        res.redirect("/" + btnValue);
+    }
 
-    LoginDetail.findOne({email: email, username: username, password: password}).then(function(foundItem){
-        if(foundItem === null){
-            res.send("<script>alert('You may have entered wrong credentials! Please check it and try again..');window.location = '/';</script>");
-        }
-        else{
-            res.render("home", {btnValue: foundItem.username});
-        }
-    });
+});
+
+app.post("/", function(req, res){
+    const btnValue = req.body.btn;
+    
+    if(btnValue == "Login"){
+        const email = req.body.email;
+        const username = req.body.username;
+        const password = req.body.password;
+
+        LoginDetail.findOne({email: email, username: username, password: password}).then(function(foundItem){
+            if(foundItem === null){
+                res.send("<script>alert('You may have entered wrong credentials! Please check it and try again..');window.location = '/';</script>");
+            }
+            else{
+                res.render("home", {btnValue: foundItem.username});
+            }
+        });
+    }
+    else{
+        const name = req.body.personName;
+        const email = req.body.email;
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const details = new LoginDetail({
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        });
+
+        LoginDetail.findOne({email: email, username: username}).then(function(foundItem){
+            if(!foundItem){
+                res.render("home", {btnValue: details.username});
+                details.save();
+            }
+            else{
+            res.send("<script>alert('Username or e-mail already in use! Try using another!');window.location = '/';</script>");
+            }
+        });
+    }
 });
 
 app.post("/quiz", function(req, res){
     res.sendFile(__dirname + "/quiz.html");
+});
+
+app.post("/exitQuiz", function(req, res){
+    const userName = req.body.button;
+    console.log(userName);
+    // res.redirect("/");
 });
 
 app.listen(3000, function(){
