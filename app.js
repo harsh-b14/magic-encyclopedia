f60f1f03112226f6189584d0a63229dec1e6d0fb
Harsh Bhanderi <hbhanderi148@gmial.com>
2023-07-23 11:30:05 +0530

    adding JWT in to cookies and verifying it

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//importing required modules
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -6,51 +8,72 @@ const async = require("async");
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const SECRET_KEY = "ITISSECRET";
+const cookieParser = require('cookie-parser')
 
+// making an express application
 const app = express();
 
+const port = process.env.PORT || 3000;
+
+// letting express app use other module properties
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
 app.use(express.json());
-app.use((req, res, next)=>{
-    console.log("HTTP Method - " + req.method + " , URL - " + req.url);
-    next();
-});
+app.use(cookieParser());
+// app.use((req, res, next)=>{
+//     console.log("HTTP Method - " + req.method + " , URL - " + req.url);
+//     next();
+// });
 
-main().catch(err => console.log(err));
- 
+// connecting to database
+main().catch(err => console.log(err)); 
 async function main() {
-  // Use connect method to connect to the server
   await mongoose.connect("mongodb://127.0.0.1:27017/wizardingWorldDB");
 }
 
+// creating an authorization middleware
+const auth = async(req, res, next) => {
+    try {
+        const token = req.cookies.userCookie;
+        const details = token.split(" ")[1];
+        const verifyUser = await jwt.verify(token, process.env.SECRET_KEY);
+        // console.log(verifyUser);
+        const user = await LoginDetail.findOne({_id: verifyUser._id});
+        console.log(user);
+        next();
+    } catch (error) {
+        res.status(401).send(error);
+    }
+}
+
+// creaeting schema to store credential of users in DB
 const signupSchema = new mongoose.Schema({
     name : String,
     email: {type: String, unique: true},
     username: {type: String, unique: true},
     password: String,
 });
-  
 const LoginDetail = new mongoose.model("LoginDetail", signupSchema);
 
+// creating schema to store info magical creatures into DB
 const creaturesSchema = new mongoose.Schema({
     name: {type: String, unique: true},
     description: String,
 });
-
 const Creature = new mongoose.model("Creature", creaturesSchema);
 
+// creating schema to store favourites of user into DB
 const favSchema = new mongoose.Schema({
     name: {type: mongoose.Schema.Types.ObjectId, ref: 'LoginDetail', unique: true},
     favourites:[{type: mongoose.Schema.Types.ObjectId, ref: 'Creature', unique: true}],
 });
-
 const Favourite = new mongoose.model("Favourite", favSchema);
 
+// all the get method 
+
 app.get("/", async (req, res) => {
-        await res.render("home", { btnValue: "Login" });
+        res.render("home", { btnValue: "Login" });
     });
 
 app.get("/login", function(req, res){
@@ -59,7 +82,6 @@ app.get("/login", function(req, res){
 
 app.get("/creatures", function(req, res){
     Creature.find({}).then(function(foundItem){
-        console.log(foundItem);
         console.log(foundItem.length);
         res.render("creature", {creatureList: foundItem});
     });
@@ -70,16 +92,21 @@ app.get("/:userName", function(req, res){
     res.send("hello " + userName);
 });
 
-app.post("/login", function(req, res){
-    const btnValue = req.body.button;
-    if(btnValue == "Login"){
+// all the post method
+
+app.post("/login", async (req, res)=>{
+    const btnValue = req.body.btn;
+    console.log(btnValue);
+    if(btnValue === "Login"){
         res.redirect("/login");
     }
     else{
+        // will display user profile page
         res.redirect("/" + btnValue);
     }
 });
 
+// post method to login or signup using JWT
 app.post("/", async (req, res) => {
         const btnValue = req.body.btn;
         if (btnValue == "Login") {
@@ -88,14 +115,6 @@ app.post("/", async (req, res) => {
             const password = req.body.password; 
 
             const existingUser = await LoginDetail.findOne({ email: email, username: username});
-            // then(function (foundItem) {
-            //     if (foundItem === null) {
-            //         res.send("<script>alert('You may have entered wrong credentials! Please check it and try again..');window.location = '/';</script>");
-            //     }
-            //     else {
-            //         res.render("home", { btnValue: foundItem.username });
-            //     }
-            // });
             if(existingUser === null){
                 res.send("<script>alert('Username of e-mail Id does not exist!');window.location = '/';</script>");
             }   
@@ -103,8 +122,13 @@ app.post("/", async (req, res) => {
             if(!matchPassword){
                 res.send("<script>alert('Wrong Password!');window.location = '/';</script>");
             }
-            const token = jwt.sign({ email: existingUser.email, username: existingUser.username, id: existingUser._id }, SECRET_KEY);
+            const token = await jwt.sign({ email: existingUser.email, username: existingUser.username, _id: existingUser._id }, process.env.SECRET_KEY, {
+                expiresIn: '1h'
+            });
             // console.log(existingUser);
+            res.cookie("userCookie", token);
+            // console.log(req.cookies.userCookie);
+            // console.log(token);
             res.render("home", { btnValue: existingUser.username });
         }
         else {
@@ -114,19 +138,22 @@ app.post("/", async (req, res) => {
             const password = req.body.password;
 
             const foundItem = await LoginDetail.findOne({ email: email });
-            if (!foundItem) {
+            if (foundItem) {
                 res.send("<script>alert('Username or e-mail already in use! Try using another!');window.location = '/';</script>");
             }
-            const hashedPassword = await bcrypt.hash(password, 10);
+            const hashedPassword = await bcrypt.hash(password, 12);
             const details = await LoginDetail.create({
                 name: name,
                 email: email,
                 username: username,
                 password: hashedPassword
             });
-            const token = jwt.sign({ email: details.email, username: details.username, id: details._id }, SECRET_KEY);
+            const token = jwt.sign({ email: details.email, username: details.username, _id: details._id }, process.env.SECRET_KEY,{
+                expiresIn: '1h'
+            });
             // res.status(201).json({user: details, token: token});
             res.render("home", { btnValue: details.username });
+            // console.log(token);
             // details.save();
         }
     });
@@ -150,12 +177,12 @@ app.post("/xyz", function(req, res){
     });
 });
 
-app.post("/favs", function(req, res){
+app.post("/favs", auth, function(req, res){
     const checkItemId = req.body.checkbox;
     // const btnValue = req.body.btnValue;
     // console.log(btnValue);
-    var newFav = new Favourite;
-    newFav.
+    // var newFav = new Favourite;
+    // newFav.
     console.log(checkItemId);
 });
 
@@ -165,8 +192,8 @@ app.post("/exitQuiz", function(req, res){
     // res.redirect("/");
 });
 
-app.listen(3000, function(){
-    console.log("server running on 3000");
+app.listen(port, function(){
+    console.log("server running on " + port);
     Creature.find({}).then(function(foundItem){
         if(foundItem.length == 0){
             Creature.insertMany(creatures).then(function(err){
